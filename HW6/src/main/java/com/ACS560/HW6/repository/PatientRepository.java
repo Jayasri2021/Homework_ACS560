cdbd9fe1cdfb76ff63ad9179214c015c2fd5ad76
pfw_Jayasri_Suresh_Vani <102977678+Jayasri2021@users.noreply.github.com>
2024-11-10 17:58:34 -0500

    Delete HW6 directory

--- HW6/src/main/java/com/ACS560/HW6/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ACS560.HW6.repository;
-
-import com.ACS560.HW6.entity.PatientEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface PatientRepository extends JpaRepository<PatientEntity, Long> {
-    Optional<PatientEntity> findByName(String name);
-}
